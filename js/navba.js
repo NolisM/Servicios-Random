9afe11c7289b4dfb110d563e183ccbc6aee17b26
Aylen <aylenstutz2016@gmail.com>
2024-05-16 23:15:53 -0300

    borre menuburger,footer peque

--- js/navba.js
+++
@@ -1,6 +0,0 @@
-const mobileMenu = document.querySelector('.mobile-menu');
-const navLinks = document.querySelector('.nav-links');
-
-mobileMenu.addEventListener('click', () => {
-  navLinks.classList.toggle('active'); // Alternar clase 'active' en clic
-});
