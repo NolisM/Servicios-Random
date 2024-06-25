366b35db93f88dfcabddc3288cf879be5ed9828e
nolismaldonado <nolis51maldonado@gmail.com>
2024-06-25 16:55:37 -0300

    estilado admin

--- js/admin.js
+++ js/admin.js
@@ -30,26 +30,76 @@ document.addEventListener('DOMContentLoaded', function () {
     ];
 
     const tbody = document.getElementById('consultasTable').getElementsByTagName('tbody')[0];
+    const main = document.querySelector('main');
 
-    consultas.forEach(consulta => {
-        let row = tbody.insertRow();
+    function mostrarMensajeNoConsultas() {
+        const mensaje = document.createElement('p');
+        mensaje.id = 'noConsultas';
+        mensaje.textContent = 'No hay consultas que mostrar.';
+        main.appendChild(mensaje);
+    }
 
-        let cellConsulta = row.insertCell(0);
-        let cellNombre = row.insertCell(1);
-        let cellApellido = row.insertCell(2);
-        let cellEmail = row.insertCell(3);
-        let cellPhone = row.insertCell(4);
-        let cellMensaje = row.insertCell(5);
-        let cellSuscripcion = row.insertCell(6);
+    function cargarConsultas() {
+        consultas.forEach((consulta, index) => {
+            let row = tbody.insertRow();
 
-        cellConsulta.textContent = consulta.consulta;
-        cellNombre.textContent = consulta.nombre;
-        cellApellido.textContent = consulta.apellido;
-        cellEmail.textContent = consulta.email;
-        cellPhone.textContent = consulta.phone;
-        cellMensaje.textContent = consulta.mensaje;
-        cellSuscripcion.textContent = consulta.suscripcion ? 'Sí' : 'No';
-    });
+            let cellConsulta = row.insertCell(0);
+            let cellNombre = row.insertCell(1);
+            let cellApellido = row.insertCell(2);
+            let cellEmail = row.insertCell(3);
+            let cellPhone = row.insertCell(4);
+            let cellMensaje = row.insertCell(5);
+            let cellSuscripcion = row.insertCell(6);
+            let cellAcciones = row.insertCell(7);
+
+            cellConsulta.textContent = consulta.consulta;
+            cellNombre.textContent = consulta.nombre;
+            cellApellido.textContent = consulta.apellido;
+            cellEmail.textContent = consulta.email;
+            cellPhone.textContent = consulta.phone;
+            cellMensaje.textContent = consulta.mensaje;
+            cellSuscripcion.textContent = consulta.suscripcion ? 'Sí' : 'No';
+
+            let deleteIcon = document.createElement('i');
+            deleteIcon.classList.add('fa', 'fa-trash', 'delete-icon');
+            deleteIcon.setAttribute('data-index', index);
+            deleteIcon.addEventListener('click', function () {
+                Swal.fire({
+                    title: '¿Estás seguro?',
+                    text: "No podrás revertir esta acción",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#0a1b27',
+                    cancelButtonColor: '#0a1b27',
+                    confirmButtonText: 'Sí, eliminarlo',
+                    cancelButtonText: 'Cancelar'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        tbody.deleteRow(row.rowIndex - 1);
+                        Swal.fire(
+                            'Eliminado!',
+                            'La consulta ha sido eliminada.',
+                            'success'
+                        );
+
+                        if (tbody.rows.length === 0) {
+                            mostrarMensajeNoConsultas();
+                        }
+                    }
+                });
+            });
+
+            cellAcciones.appendChild(deleteIcon);
+        });
+
+        if (tbody.rows.length === 0) {
+            mostrarMensajeNoConsultas();
+        }
+    }
+
+    cargarConsultas();
+
+
 });
 
 
